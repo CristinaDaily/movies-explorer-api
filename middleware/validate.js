d97a1f30db2c04f645525e833f609ac90170c497
Cristina <kristi.chichkanova@gmail.com>
2023-11-13 11:59:58 +0100

    bagfix: validateUserUpdate can update only email and name

--- middleware/validate.js
+++ middleware/validate.js
@@ -20,7 +20,6 @@ const validateLoginData = celebrate({
 const validateUserUpdate = celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().optional(),
-    password: Joi.string().optional(),
     name: Joi.string().min(2).max(30).optional(),
   }),
 });
